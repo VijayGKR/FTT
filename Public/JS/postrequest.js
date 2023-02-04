5fb9a967609422af5d9c92454f28175d11277d25
VijayGKR <vkumarav@usc.edu>
2023-02-04 12:55:17 -0800

    More test changes

--- Public/JS/postrequest.js
+++ Public/JS/postrequest.js
@@ -1,36 +1,46 @@
-$(document).ready(function () { 
-    // SUBMIT FORM 
+$(document).ready(function () {
+
+    // SUBMIT FORM
     $("#weatherForm").submit( (event) => {
-    // Prevent the form from submitting via the browser. 
-    event.preventDefault(); 
-    ajaxPost();
+        // Prevent the form from submitting via the browser.
+        event.preventDefault();
+        ajaxPost();
     });
-    function ajaxPost() { 
-    // PREPARE FORM DATA
-    var formData = {
-     cityname: $("#cityname").val(),
+
+
+    function ajaxPost() {
+
+        // PREPARE FORM DATA
+        var formData = {
+            cityname: $("#cityname").val(),
+           // lastname: $("#lastname").val()
+        }
+
+        // DO POST
+        $.ajax({
+            type: "POST",
+            contentType: "application/json",
+            url: window.location + "api/customers/save",
+            data: JSON.stringify(formData),
+            dataType: 'json',
+            success:  (output)=> {
+                $("#postResultDiv").html("<p>" +
+                    "<br>" + 
+                    //.replace(/\"([^"]+)\":/g, "$1:")
+                    "" + JSON.stringify(`<code> ${output.addname} </code> Current Tempeature is <code>${output.temp}<sup>o</sup>C </code>`) + "</p>");
+            },
+            error:  (e) => {
+                alert("Error!");
+                console.log("ERROR: ", e);
+            }
+        });
+        // Reset FormData after Posting
+        resetData();
+
     }
-    // DO POST 
-    $.ajax({
-    type: "POST",
-     contentType: "application/json",
-    url: window.location + "api/customers/save",
-    data: JSON.stringify(formData),
-    dataType: 'json',
-    success: (output)=> { 
-    $("#postResultDiv").html("<p>" +
-    "<br>" +
-    "" + JSON.stringify(`<code> ${output.addname} </code> Current Tempeature is <code>${output.temp}<sup>o</sup>C </code>`) + "</p>"); 
-    },
-    error: (e) => {
-    alert("Error!"); 
-    console.log("ERROR: ", e); 
-    }
-    }); 
-    // Reset FormData after Posting 
-    resetData(); 
-    } 
+
     function resetData() {
-    $("#citytname").val(""); 
+        $("#citytname").val("");
+       // $("#lastname").val("");
     }
-    })
+})
