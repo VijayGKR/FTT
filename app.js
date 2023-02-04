a2f973a87e0f9c1b0448c1cd1765e23df9b25330
VijayGKR <vijaygkr1@gmail.com>
2023-02-04 11:32:42 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+let todos= ["Hello World"]
+
+app.get('/todos',(req,res) => {
+    res.send(todos);
+})
+
+app.post("/todos",(req,res) => {
+    const name = req.body.name;
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
