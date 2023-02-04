2317e4b1aabc847b5886841736940c55bd88f150
VijayGKR <vkumarav@usc.edu>
2023-02-04 12:44:35 -0800

    Weather Test Branch 2

---
+++ WeatherAPI/getWeather.js
@@ -0,0 +1,21 @@
+////weather api call
+const request =require('request'); 
+var getWeather = (lat,lon,callback) => { 
+request ({ 
+url: `​https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=99c0c154df150b87ca494f16de94eb68`, 
+json: true
+},(error,response,body)=>
+{
+ if(!error && response.statusCode === 200) 
+{ 
+callback(undefined,{ 
+Temp: body.main.temp 
+});
+} 
+else {
+callback('unable to fetch weather'); 
+} 
+}); 
+}; 
+module.exports.getWeather = getWeather; 
+////
--- app.js
+++ app.js
@@ -1,19 +1,66 @@
-const express = require('express')
-const app = express()
-const port = 3000
-let todos= ["Hello World"]
+const geocode = require('./geocode/geocode')
+const getWeather = require('./WeatherApi/getWeather')
+const port = process.env.PORT || 8081;
+var express = require("express");
+var app = express();
 
-app.get('/todos',(req,res) => {
-    res.send(todos);
+var router = express.Router();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+var path = __dirname + '/views/';
+
+router.use(function (req, res, next) {
+    console.log("/" + req.method);
+    next();
+});
+
+var outputs =[];
+app.get("/", function (req, res) {
+    res.sendFile(path + "index.html");
+});
+
+app.post("/api/customers/save", function (req, res) {
+    console.log('Post a Weather: ' + JSON.stringify(req.body));
+    var cityAddress = req.body.cityname;
+    //
+      geocode.getAddress(cityAddress, (errorMessage, results) => {
+          if (errorMessage) {
+              console.log(errorMessage);
+             return res.status(400).send(errorMessage);
+          } else {
+              var add = results.address;
+              
+              //Get Weather 
+              getWeather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
+                  if (errorMessage) {
+                      console.log(errorMessage);
+                      return res.status(400).send(errorMessage);
+                  } else {
+                      var TempCel = weatherResults.Temp - 273.15;
+                      var Temperature = TempCel.toFixed(0);
+                      var output = {};
+                      output.temp = Temperature;
+                      output.addname = add.replace(/ *\,[^,]*\ */g, "");
+                        outputs.push(output);
+                        console.log(output);
+                      return res.status(200).send(output);
+                  }
+              });
+
+          }
+      });
+});
+
+app.use("/", router);
+
+app.use("*", function (req, res) {
+    res.sendFile(path + "404.html");
+});
+
+app.listen(port, function () {
+    console.log(`Example app listening on port localhost: ${port} `);
 })
-
-app.post("/todos",(req,res) => {
-    const name = req.body.name;
-})
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
-
-app.fetch()
-//hello test branch
